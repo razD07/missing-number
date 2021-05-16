7a444016717985648564203c896e09ac26bd7716
razD07 <84086832+razD07@users.noreply.github.com>
2021-05-16 22:32:59 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class GFG
+{
+
+    static int getMissingNo(int[] a, int n)
+    {
+        int total = (n + 1) * (n + 2) / 2;
+
+        for (int i = 0; i < n; i++)
+            total -= a[i];
+
+        return total;
+    }
+
+    public static void Main()
+    {
+        int[] a = { 1, 2, 3, 5, 6, 7 };
+        int miss = getMissingNo(a, 6);
+        Console.Write(miss);
+    }
+}
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
